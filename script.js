eaec68c84ecc86a5156541eb1caaf912e5240ac7
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2021-12-02 16:36:42 -0300

    add email verification

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const changeSrc = (element, src) => {
 }
 
 function resizeHeroImage(width) {
-
+    
     if (width >= 992) {
         changeSrc(".hero img", "images/hero-desktop.jpg");
     } else {
@@ -17,5 +17,30 @@ function resizeHeroImage(width) {
 
 d.addEventListener("DOMContentLoaded", (e) => { resizeHeroImage(e.target.defaultView.innerWidth) });
 window.addEventListener("resize", (e) => { resizeHeroImage(e.target.innerWidth) });
+
+
+// Email verification
+
+const $input = d.querySelector("input[type=email]"),
+$btn = d.querySelector(".btn-input"),
+$textError = d.querySelector(".text-error-input"),
+$iconError = d.querySelector(".icon-error-input"),
+regexEmail = /(^$|^.*@.*\..*$)/;
+
+$btn.addEventListener("click", checkEmail);
+$input.addEventListener("keydown", checkEmail);
+
+function checkEmail() {
+    if ($input.value == "" || !regexEmail.test($input.value)) {
+        $input.classList.add("border-error");
+        $textError.classList.remove("none"); 
+        $iconError.classList.remove("none"); 
+    }else {
+        $input.classList.remove("border-error");
+        $textError.classList.add("none"); 
+        $iconError.classList.add("none"); 
+    }
+}
+
 
 
